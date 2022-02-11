81908df4d25a14d97536ae2b8ae5feae658035e7
GTsatiris <george.tsatiris@gmail.com>
2022-02-11 23:38:01 +0200

    Using Movenet, added levels and look and feel

--- sketchBACK.js
+++ sketchBACK.js
@@ -1,7 +1,6 @@
 let video;
-let detector;
 let poseNet;
-let poses;
+let poses = [];
 let currentPose;
 let canvas;
 let fontFuzzyBubbles;
@@ -10,22 +9,15 @@ let loadingImg;
 let rotationAngle;
 
 let startTimer;
-let pauseTimer;
-let pauseInterval;
-let totalPauseInterval;
 let resetTime;
-let resetPauseTime;
 
-let foundFirstPose;
 let switchFlag = false;
 // let keypointIndexes = [9, 10, 15, 16];
 let modelIsReady = false;
 let hasPose = false;
-let videoIsReady = false;
 
-let minConf = 0.3;
 let tolerance = 50;
-let windowSize = 10;
+let windowSize = 20;
 // let levelChecks = [false, false, false];
 
 // let rWrist;
@@ -49,10 +41,9 @@ let currentJoints = new Array(17);
 // ];
 
 function translateToNewDim(point) {
-  var p = {x:0, y:0, score:0.0};
+  var p = {x:0, y:0};
   p.x = (point.x * windowWidth)/video.width;
   p.y = (point.y * windowHeight)/video.height;
-  p.score = point.score;
   return p;
 }
 
@@ -92,42 +83,7 @@ function switchCamera()
   video = createCapture(options);
 }
 
-async function initialize() {
-  detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet);
-  // detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, {modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER});
-  modelIsReady = true;
-}
-
-async function videoReady() {
-  videoIsReady = true;
-  await getPoses();
-}
-
-async function getPoses() {
-  if(modelIsReady)
-  {
-    poses = await detector.estimatePoses(video.elt);
-    currentPose = poses[0];
-    if (typeof currentPose !== 'undefined') {
-      if (typeof currentPose.keypoints !== 'undefined') {
-        hasPose = true;
-        updateKeypoints();
-        smoothAndTranslate();
-      }
-      else
-      {
-        hasPose = false;
-      }
-    }
-    else
-    {
-      hasPose = false;
-    }
-  }
-  setTimeout(getPoses, 0);
-}
-
-async function preload() {
+function preload() {
   fontFuzzyBubbles = loadFont('assets/fonts/FuzzyBubbles-Regular.ttf');
   fontFuzzyBubblesBOLD = loadFont('assets/fonts/FuzzyBubbles-Bold.ttf');
   loadingImg = loadImage('assets/img/1544764567.png');
@@ -136,16 +92,10 @@ async function preload() {
   for (var i = 0; i < windows.length; i++) {
     windows[i] = [];
   }
-  
-  await initialize();
 }
 
 function setup() {
-  foundFirstPose = false;
   resetTime = true;
-  resetPauseTime = true;
-  pauseInterval = 0;
-  totalPauseInterval = 0;
   rotationAngle = 0;
   let params = new URLSearchParams(location.search);
   currentLevel = parseInt(params.get('lvl'));
@@ -155,8 +105,7 @@ function setup() {
   centerCanvas();
   // canvas.center('horizontal');
   // canvas.center('vertical');
-
-  video = createCapture(VIDEO, videoReady);
+  video = createCapture(VIDEO);
   // var constraints = {
   //   video: {
   //     facingMode: {
@@ -168,9 +117,36 @@ function setup() {
   //   //} 
   // };
   // video = createCapture(constraints);
-  video.size(640, 480);
+  video.size(320, 240);
 
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on("pose", function(results) {
+    poses = results;
+    currentPose = poses[0];
+    if (typeof currentPose !== 'undefined') {
+      if (typeof currentPose.pose !== 'undefined') {
+        hasPose = true;
+      }
+      else
+      {
+        hasPose = false;
+      }
+    }
+    else
+    {
+      hasPose = false;
+    }
+  });
+  // Hide the video element, and just show the canvas
   video.hide();
+}
+
+function modelReady() {
+  modelIsReady = true;
+  console.log("Model Ready");
 }
 
 function deviceTurned() {
@@ -184,7 +160,7 @@ function windowResized() {
 } 
 
 function draw() {
-  if(modelIsReady && levelsReady && videoIsReady)
+  if(modelIsReady && levelsReady)
   {
     frameRate(30);
 
@@ -195,23 +171,31 @@ function draw() {
     scale(-1, 1);
     image(video, 0, 0, width, height);
 
-    drawLevelPoints();
-
+    // We can call both functions to draw all keypoints and the skeletons
     if(hasPose)
     {
-      foundFirstPose = true;
-
       if(resetTime)
       {
         resetTime = false;
         startTimer = millis();
       }
+            
+      updateKeypoints();
+
+      smoothAndTranslate();
+
+      drawLevelPoints();
 
       drawKeypoints();
 
       checkPoints();
       checkProgress();
 
+      // for (let i = 0; i < curr; i++) {
+      //   checkPointI(i);
+      // }
+
+      //if(levelChecks[0] && levelChecks[1] && levelChecks[2])
       if(canProgress)
       {
         //if(currentLevel < 2){
@@ -232,47 +216,20 @@ function draw() {
     }
     pop();
     
-    let millisec;
-
     textFont(fontFuzzyBubblesBOLD);
     textSize(windowHeight/12);
     strokeWeight(windowHeight/120);
     stroke(51);
-    
-    if(!hasPose)
-    {
-      if(foundFirstPose)
-      {
-        if(resetPauseTime)
-        {
-          resetPauseTime = false;
-          pauseTimer = millis();
-        }
-        pauseInterval = millis() - pauseTimer;
-      }
-      push();
-      fill(255, 0, 0);
-      textAlign(CENTER, CENTER);
-      text('NO POSE DETECTED!', windowWidth/2.0, windowHeight/2.0);
-      pop()
-    }
-    else
-    {
-      resetPauseTime = true;
-      totalPauseInterval += pauseInterval;
-      pauseInterval = 0;
-      // milliSec = millisToMinAndSec(millis() - startTimer);
-      milliSec = msToTime(millis() - startTimer - totalPauseInterval);
-    }
     fill(255, 255, 255);
     text('Level ' + (currentLevel + 1), 10, 80);
 
+    // milliSec = millisToMinAndSec(millis() - startTimer);
+    milliSec = msToTime(millis() - startTimer);
     
-    if((startTimer != null) && (milliSec != null))
+    if(startTimer != null)
     {
       text(milliSec, windowWidth*0.75, 80);
     }
-    
   }
   else
   {
@@ -293,15 +250,12 @@ function updateKeypoints() {
   for(var i = 0; i < currLvlData.jointNum; i++)
   {
     var jointIdx = currLvlData.trackedJoins[i];
-    if(currentPose.keypoints[jointIdx].score > minConf)
-    {
-      currentJoints[jointIdx] = currentPose.keypoints[jointIdx];
-    }
-    else
-    {
-      currentJoints[jointIdx] = {x : video.width/2.0, y : video.height/2.0, score : currentPose.keypoints[jointIdx].score};
-    }
+    currentJoints[jointIdx] = currentPose.pose.keypoints[jointIdx].position;
   }
+  // rWrist = currentPose.pose.rightWrist;
+  // lWrist = currentPose.pose.leftWrist;
+  // rAnkle = currentPose.pose.rightAnkle;
+  // lAnkle = currentPose.pose.leftAnkle;
 }
 
 function smoothAndTranslate() {
@@ -311,16 +265,21 @@ function smoothAndTranslate() {
     currentJoints[jointIdx] = translateToNewDim(currentJoints[jointIdx]);
     currentJoints[jointIdx] = avgFiltering(currentJoints[jointIdx], jointIdx);
   }
+  // rWrist = translateToNewDim(rWrist);
+  // lWrist = translateToNewDim(lWrist);
+  // rAnkle = translateToNewDim(rAnkle);
+  // lAnkle = translateToNewDim(lAnkle);
+
+  // rWrist = avgRWRIST(rWrist);
+  // lWrist = avgLWRIST(lWrist);
+  // rAnkle = avgRANKLE(rAnkle);
+  // lAnkle = avgLANKLE(lAnkle);
 }
 
 function drawKeypoints() {
   for(var i = 0; i < currLvlData.jointNum; i++)
   {
     var jointIdx = currLvlData.trackedJoins[i];
-    if(currentJoints[jointIdx].score < minConf)
-    {
-      continue;
-    }
     if(currLvlData.jointSpecific)
     {
       for(var j = 0; j < currLvlData.markerNum; j++)
@@ -340,14 +299,29 @@ function drawKeypoints() {
     noStroke();
     ellipse(currentJoints[jointIdx].x, currentJoints[jointIdx].y, 50, 50);
   }
+  // fill(255, 0, 0);
+  // noStroke();
+  // ellipse(rWrist.x, rWrist.y, 50, 50);
+
+  // fill(255, 0, 0);
+  // noStroke();
+  // ellipse(rAnkle.x, rAnkle.y, 50, 50);
+
+  // fill(255, 0, 0);
+  // noStroke();
+  // ellipse(lWrist.x, lWrist.y, 50, 50);
+
+  // fill(255, 0, 0);
+  // noStroke();
+  // ellipse(lAnkle.x, lAnkle.y, 50, 50);
 }
 
-function avgFiltering(src, idx) {
+function avgFiltering(x, idx) {
   // the first time this runs we add the current x to the array n number of times
   if (windows[idx].length < 1) {
     console.log('this should only run once');
     for (let i = 0; i < windowSize; i++) {
-      windows[idx].push(src);
+      windows[idx].push(x);
     }
   // if the number of frames to average is increased, add more to the array
   }
@@ -355,32 +329,173 @@ function avgFiltering(src, idx) {
     console.log('adding more xs');
     const moreXs = windowSize - windows[idx].length;
     for (let i = 0; i < moreXs; i++) {
-      windows[idx].push(src);
+      windows[idx].push(x);
   }
   // otherwise update only the most recent number
   } else {
     windows[idx].shift(); // removes first item from array
-    windows[idx].push(src); // adds new x to end of array
+    windows[idx].push(x); // adds new x to end of array
   }
 
   let sum_X = 0;
   let sum_Y = 0;
-  let sum_Score = 0.0;
   for (let i = 0; i < windows[idx].length; i++) {
     sum_X += windows[idx][i].x;
     sum_Y += windows[idx][i].y;
-    sum_Score += windows[idx][i].score;
   }
 
   let point = {};
   
   point.x = sum_X / windows[idx].length;
   point.y = sum_Y / windows[idx].length;
-  point.score = sum_Score / windows[idx].length;
 
   // return the average x value 
   return point;
 }
+
+// function avgRWRIST(x) {
+//   // the first time this runs we add the current x to the array n number of times
+//   if (windowRWRIST.length < 1) {
+//       console.log('this should only run once');
+//       for (let i = 0; i < windowSize; i++) {
+//           windowRWRIST.push(x);
+//       }
+//   // if the number of frames to average is increased, add more to the array
+//   } else if (windowRWRIST.length < windowSize) {
+//       console.log('adding more xs');
+//       const moreXs = windowSize - windowRWRIST.length;
+//       for (let i = 0; i < moreXs; i++) {
+//           windowRWRIST.push(x);
+//       }
+//   // otherwise update only the most recent number
+//   } else {
+//       windowRWRIST.shift(); // removes first item from array
+//       windowRWRIST.push(x); // adds new x to end of array
+//   }
+
+//   let sum_X = 0;
+//   let sum_Y = 0;
+//   for (let i = 0; i < windowRWRIST.length; i++) {
+//     sum_X += windowRWRIST[i].x;
+//     sum_Y += windowRWRIST[i].y;
+//   }
+
+//   let point = {};
+  
+//   point.x = sum_X / windowRWRIST.length;
+//   point.y = sum_Y / windowRWRIST.length;
+
+//   // return the average x value 
+//   return point;
+// }
+
+// function avgRANKLE(x) {
+//   // the first time this runs we add the current x to the array n number of times
+//   if (windowRANKLE.length < 1) {
+//       console.log('this should only run once');
+//       for (let i = 0; i < windowSize; i++) {
+//           windowRANKLE.push(x);
+//       }
+//   // if the number of frames to average is increased, add more to the array
+//   } else if (windowRANKLE.length < windowSize) {
+//       console.log('adding more xs');
+//       const moreXs = windowSize - windowRANKLE.length;
+//       for (let i = 0; i < moreXs; i++) {
+//           windowRANKLE.push(x);
+//       }
+//   // otherwise update only the most recent number
+//   } else {
+//       windowRANKLE.shift(); // removes first item from array
+//       windowRANKLE.push(x); // adds new x to end of array
+//   }
+
+//   let sum_X = 0;
+//   let sum_Y = 0;
+//   for (let i = 0; i < windowRANKLE.length; i++) {
+//     sum_X += windowRANKLE[i].x;
+//     sum_Y += windowRANKLE[i].y;
+//   }
+
+//   let point = {};
+  
+//   point.x = sum_X / windowRANKLE.length;
+//   point.y = sum_Y / windowRANKLE.length;
+
+//   // return the average x value 
+//   return point;
+// }
+
+// function avgLWRIST(x) {
+//   // the first time this runs we add the current x to the array n number of times
+//   if (windowLWRIST.length < 1) {
+//       console.log('this should only run once');
+//       for (let i = 0; i < windowSize; i++) {
+//           windowLWRIST.push(x);
+//       }
+//   // if the number of frames to average is increased, add more to the array
+//   } else if (windowLWRIST.length < windowSize) {
+//       console.log('adding more xs');
+//       const moreXs = windowSize - windowLWRIST.length;
+//       for (let i = 0; i < moreXs; i++) {
+//           windowLWRIST.push(x);
+//       }
+//   // otherwise update only the most recent number
+//   } else {
+//       windowLWRIST.shift(); // removes first item from array
+//       windowLWRIST.push(x); // adds new x to end of array
+//   }
+
+//   let sum_X = 0;
+//   let sum_Y = 0;
+//   for (let i = 0; i < windowLWRIST.length; i++) {
+//     sum_X += windowLWRIST[i].x;
+//     sum_Y += windowLWRIST[i].y;
+//   }
+
+//   let point = {};
+  
+//   point.x = sum_X / windowLWRIST.length;
+//   point.y = sum_Y / windowLWRIST.length;
+
+//   // return the average x value 
+//   return point;
+// }
+
+// function avgLANKLE(x) {
+//   // the first time this runs we add the current x to the array n number of times
+//   if (windowLANKLE.length < 1) {
+//       console.log('this should only run once');
+//       for (let i = 0; i < windowSize; i++) {
+//           windowLANKLE.push(x);
+//       }
+//   // if the number of frames to average is increased, add more to the array
+//   } else if (windowLANKLE.length < windowSize) {
+//       console.log('adding more xs');
+//       const moreXs = windowSize - windowLANKLE.length;
+//       for (let i = 0; i < moreXs; i++) {
+//           windowLANKLE.push(x);
+//       }
+//   // otherwise update only the most recent number
+//   } else {
+//       windowLANKLE.shift(); // removes first item from array
+//       windowLANKLE.push(x); // adds new x to end of array
+//   }
+
+//   let sum_X = 0;
+//   let sum_Y = 0;
+//   for (let i = 0; i < windowLANKLE.length; i++) {
+//     sum_X += windowLANKLE[i].x;
+//     sum_Y += windowLANKLE[i].y;
+//   }
+
+//   let point = {};
+  
+//   point.x = sum_X / windowLANKLE.length;
+//   point.y = sum_Y / windowLANKLE.length;
+
+//   // return the average x value 
+//   return point;
+// }
 
 function distance(a, b) {
   var dx = a.x - b.x;
@@ -427,7 +542,6 @@ function drawLevelPoints() {
 
 function checkPoints() {
   for (let i = 0; i < currLvlData.markerNum; i++) {
-    
     var trans_p = translateToNewDim(currLvlData.markerPos[i]);
     
     currLvlData.markerChecks[i] = false;
@@ -458,10 +572,6 @@ function checkPoints() {
     {
       for (let j = 0; j < currLvlData.jointNum; j++) {
         var jointIdx = currLvlData.trackedJoins[j];
-        if(currentJoints[jointIdx].score < minConf)
-        {
-          continue;
-        }
         currLvlData.markerChecks[i] = currLvlData.markerChecks[i] || (distance(trans_p, currentJoints[jointIdx]) < tolerance);
       }
     }
